7881ac181323ba2d3db5b67433dc60342880e7f6
Ferawati Hartanti Pratiwi <mpermperpisang@users.noreply.github.com>
2019-08-16 09:33:00 +0700

    GO - Initiate unit testing (#2)
    * initiate automation
    
    * add unit testing and update readme

--- initiate_test.go
+++ initiate_test.go
@@ -12,6 +12,9 @@ func InitiateBrowser(s *godog.Suite) {
 	s.Step(`^visit ios$`, step_definitions.OpenIOS)
 	s.Step(`^client sends a ([^\"]*) request to "([^\"]*)"$`, step_definitions.RequestAPI)
 	s.Step(`^response status should be "([^\"]*)"$`, step_definitions.ResponseAPI)
+	s.Step(`^user has a name "([^\"]*)"$`, step_definitions.GivenUserName)
+	s.Step(`^Testivus meet user$`, step_definitions.MeetUserName)
+	s.Step(`^Testivus say "([^\"]*)"$`, step_definitions.SayHelloName)
 
 	GodogMainSupport(s)
 }
