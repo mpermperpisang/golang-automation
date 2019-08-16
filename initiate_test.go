f4287fafac962a187e768b1d72603fbf67554cb3
Ferawati Hartanti Pratiwi <mpermperpisang@users.noreply.github.com>
2019-08-16 09:41:35 +0700

    Update initiate_test.go

--- initiate_test.go
+++ initiate_test.go
@@ -5,7 +5,7 @@ import (
 	"github.com/golang-automation/features/step_definitions"
 )
 
-func InitiateBrowser(s *godog.Suite) {
+func InitiateAutomation(s *godog.Suite) {
 	s.Step(`^visit dweb$`, step_definitions.OpenDWEB)
 	s.Step(`^visit mweb$`, step_definitions.OpenMWEB)
 	s.Step(`^visit android$`, step_definitions.OpenAndroid)
