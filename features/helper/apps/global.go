f098bacd570f68f5b7e311ac507624f0ff5d0c30
Ferawati Hartanti Pratiwi <ferawati.10109266@gmail.com>
2019-08-19 19:52:31 +0700

    add comment

--- features/helper/apps/global.go
+++ features/helper/apps/global.go
@@ -6,8 +6,10 @@ import (
 	"github.com/sclevine/agouti/appium"
 )
 
+/*Device is a device variable for appium*/
 var Device *appium.Device
 
+/*ResetApplication is a function for reset app*/
 func ResetApplication() error {
 	if err := Device.Reset(); err != nil {
 		log.Fatalln(err)
@@ -16,6 +18,7 @@ func ResetApplication() error {
 	return nil
 }
 
+/*InstallApplication is a function for install app*/
 func InstallApplication(app string) error {
 	if err := Device.InstallApp(app); err != nil {
 		log.Fatalln(err)
@@ -24,6 +27,7 @@ func InstallApplication(app string) error {
 	return nil
 }
 
+/*LaunchApplication is a function for launch app*/
 func LaunchApplication() error {
 	if err := Device.LaunchApp(); err != nil {
 		log.Fatalln(err)
@@ -32,6 +36,7 @@ func LaunchApplication() error {
 	return nil
 }
 
+/*CloseApplication is a function for close app*/
 func CloseApplication() error {
 	if err := Device.CloseApp(); err != nil {
 		log.Fatalln(err)
