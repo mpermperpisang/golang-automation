d6e0fb56ded634e7e5cedff1b47ce5dcdd66f683
Ferawati Hartanti Pratiwi <ferawati.10109266@gmail.com>
2019-09-02 23:57:38 +0700

    revert BaseURL

--- features/helper/web/base.go
+++ features/helper/web/base.go
@@ -9,9 +9,6 @@ import (
 /*Driver is global variable*/
 var Driver selenium.WebDriver
 
-/*BaseURL is global variable*/
-var BaseURL string
-
 /*DriverConnect is function for connect to driver*/
 func DriverConnect() error {
 	caps := selenium.Capabilities{"browserName": os.Getenv("BROWSER")}
@@ -37,7 +34,7 @@ func GoToURL(url string) error {
 
 /*GoToDWEBURL is function to access dweb url*/
 func GoToDWEBURL(url string) error {
-	BaseURL = os.Getenv("DWEB_BASE_URL") + url
+	BaseURL := os.Getenv("DWEB_BASE_URL") + url
 	website := Driver.Get(BaseURL)
 
 	return website
@@ -45,7 +42,7 @@ func GoToDWEBURL(url string) error {
 
 /*GoToMWEBURL is function to access mweb url*/
 func GoToMWEBURL(url string) error {
-	BaseURL = os.Getenv("MWEB_BASE_URL") + url
+	BaseURL := os.Getenv("MWEB_BASE_URL") + url
 	website := Driver.Get(BaseURL)
 
 	return website
