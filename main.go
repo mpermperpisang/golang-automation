e1cd70dcb3fc865ad6a0ca3a82012be23130b80a
Ferawati Hartanti Pratiwi <20101521+mpermperpisang@users.noreply.github.com>
2020-03-17 22:14:57 +0700

    GO - add browser name (#22)
    * add browser name
    
    * add danger
    
    * gemfile

--- main.go
+++ main.go
@@ -3,7 +3,9 @@ package main
 import (
 	"fmt"
 	"log"
+	"os"
 
+	"github.com/bdotdub/danger-go"
 	"github.com/joho/godotenv"
 )
 
@@ -14,5 +16,11 @@ func init() {
 }
 
 func main() {
-	// can be blank for now
+	_, results, err := danger.Danger()
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer results.Flush(os.Stdout)
+
+	results.Message("✌️ Howdy!")
 }
