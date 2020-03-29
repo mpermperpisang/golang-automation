2af22b17333d446ec433d926c5c28804f4a1a06f
mpermper321 <62696139+mpermper321@users.noreply.github.com>
2020-03-29 16:18:42 +0000

    Update env.go

--- features/support/env.go
+++ features/support/env.go
@@ -24,7 +24,6 @@ func GodogMainSupport(s *godog.Suite) {
 		fmt.Println("Starting automation")
 		fmt.Println("Running scenario with tag : " + tag)
 	})
-	
 
 	s.AfterScenario(func(interface{}, error) {
 		if web.Driver != nil {
