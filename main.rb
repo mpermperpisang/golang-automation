f6879cc2d49f0f2bcaa109e3e27acac800f8cfe0
Ferawati Hartanti Pratiwi <20101521+mpermperpisang@users.noreply.github.com>
2020-03-30 21:19:00 +0700

    Create main.rb

---
+++ main.rb
@@ -0,0 +1,13 @@
+# frozen_string_literal: true
+
+require 'report_builder'
+
+options = {
+  report_title: 'Smoke Testing Report',
+  report_types: ['json', 'html']
+}
+
+ReportBuilder.build_report options
+
+
+
