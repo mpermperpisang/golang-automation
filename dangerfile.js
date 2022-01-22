8d4ad185aed52f2acbfbc89efce4268a908aa60a
Ferawati Hartanti Pratiwi <ferawati.10109266@gmail.com>
2022-01-22 14:20:23 +0700

    feat: add danger js

---
+++ dangerfile.js
@@ -0,0 +1,11 @@
+import {
+  prTitle, prDesc, prReviewers, prAssignees, prLabels, prChangesCount, prCommits,
+} from 'danger-js-github';
+
+prTitle();
+prDesc();
+prReviewers();
+prAssignees();
+prLabels();
+prChangesCount();
+prCommits();
